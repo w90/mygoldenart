414dce23f3d0383c6f60255085658bab63636319
Leszek Rutkowski <leszek@tylko.com>
2017-07-03 09:46:52 +0200

    New modules

---
+++ hello.js
@@ -0,0 +1,5 @@
+console.log('hello');
+
+var url = require('url');
+
+console.log( url.parse( 'http://mygoldenart.com' ) );
---
+++ logger.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const chalk = require( chalk );
+
+console.log( chalk.green.bold( 'Zielony pogrubiony tekst' ) );
